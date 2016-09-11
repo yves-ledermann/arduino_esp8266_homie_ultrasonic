cec3c52799a4054f539496acbaa6e4f2a8a057bd
Yves Ledermann <yves@laedis.ch>
2016-09-11 15:26:43 +0200

    WIP

--- arduino_esp8266_homie_ultrasonicMeter/main.h
+++ arduino_esp8266_homie_ultrasonicMeter/main.h
@@ -9,22 +9,19 @@
 #include "Arduino.h"
 //add your includes for the project arduino_esp8266_homie_ultrasonicMeter here
 
+// Homie Framework
 #include <homie.h>
 
+// Homie Nodes
+#include "D:\Repos\home-automation\homie-nodes\sensor-ultrasonic\node-sensor-ultrasonic.h"
 
 #include <SoftwareSerial.h>
-//#include <ArduinoJson.h> 	// min V 5.0.8
-//#include <Bounce2.h> 		// min V 2.0
-#include "arduino_OTA.h"
 
+#include "arduino_OTA.h"
 
 //end of add your includes here
 
-
 //add your function definitions for the project arduino_esp8266_homie_ultrasonicMeter here
-
-
-
 
 //Do not add code below this line
 #endif /* _arduino_esp8266_homie_ultrasonicMeter_H_ */
